3bba4893a782e2b28cca4c5f05b610fc4c92a6da
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-02-06 03:02:33 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import http from 'http';
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.end('Hello world')
+})
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+} )
