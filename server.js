d53ff3813256bd205f8b84a00c6c2ac6a776fa03
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-02-06 14:53:54 +0100

    sent a response with the write metod

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 import http from 'http';
-import fs from fs/promises
-const PORT = process.env.PORT
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path'
+const PORT = process.env.PORT;
 
+// Get current path
+// These are not available in the ES modules environment tho
 
-const server = http.createServer((req, res) => {
+// __filename
+// __dirname
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// console.log(__filename)
+// console.log(__dirname)
+
+const server = http.createServer ( async(req, res) => {
 
     try {
         if (req.method === 'GET') {
+            let filePath;
             if (req.url === "/") {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end('<h1>Home Page</h1>')
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if (req.url === "/about") {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end('<h1>About Page</h1>')
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {
+                throw new Error('Not Found')
             }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         }
         else {
             throw new Error('Method not allowed');
---
+++ server2.js
@@ -0,0 +1,7 @@
+import {createServer} from 'http';
+
+const users = [
+    {id: 1, name: 'John Doe'},
+    {id: 2, name: 'Jane Doe'},
+    {id: 1, name: 'Jim Doe'}
+]
